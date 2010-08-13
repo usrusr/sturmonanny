d2d32afad82eaf3a60fe08722284dd0e61180ce4
usrusr <github@spam.ulf-schreiber.de>
2010-08-14 00:58:10 +0200

    dom helper initial version

---
+++ src/main/scala/de/immaterialien/qlmap/sprites/DomHelper.scala
@@ -0,0 +1,109 @@
+package de.immaterialien.qlmap.sprites
+
+import scala.collection._
+import org.apache.batik
+import batik.dom.svg
+import org.apache.batik.ext.awt.image.renderable._
+import org.apache.batik.gvt.filter._
+import java.awt.image.renderable._
+import org.w3c.dom._
+import org.w3c.dom.svg._
+
+object DomHelper {
+  //  implicit def nodlister(nl:NodeList):Seq[Element]={
+  //    new Seq[Element]{
+  //      
+  //    }
+  //    List()
+  //  }
+
+  class WrapElement(e: Element, doc: SVGDocument) {
+    def children(name: (Element => Boolean)): IndexedSeq[Element] = {
+      val cs = e.getChildNodes
+      val l = cs.getLength
+      val ll = for (i <- 0 until l) yield {
+        val n = cs.item(i)
+        if (n.getNodeType == Node.ELEMENT_NODE) {
+          val e = n.asInstanceOf[Element]
+          if (name(e)) Some(e) else None
+        } else None
+      }
+      ll flatten 
+    }
+    def domElement = e
+    def append(es: Element*) = {
+      for (c <- es) e.appendChild(c)
+      this
+    }
+    def element(name: String, args: (String, String)*): Element = DomHelper.element(doc, name, args: _*)
+
+    def append(name: String, args: (String, String)*) = {
+      val c = element(name, args: _*)
+      e.appendChild(c)
+      c
+    }
+
+    def firstElement(name: (Element => Boolean)): Option[Element] = {
+      val cs = children(name)
+      if (cs.length > 0) Some(cs.first) else None
+    }
+    def appendBefore(name: (Element => Boolean), es: Element*) = {
+      firstElement(name).map { f =>
+        for (c <- es.reverse) {
+          println("f is " + f)
+          println("e is " + e)
+          println("c is " + c)
+          e.insertBefore(c, f)
+        }
+      } orElse {
+        for (c <- es) e.appendChild(c)
+        None
+      }
+      this
+    }
+  }
+
+  def element(doc: SVGDocument, nsName: (String, String), args: (String, String)*) = {
+    val e = doc.createElementNS(nsName._1, nsName._2)
+    for ((n, v) <- args) e.setAttribute(n, v)
+    e
+  }
+  def element(doc: SVGDocument, name: String, args: (String, String)*) = {
+    val e = doc.createElement(name)
+    for ((n, v) <- args) e.setAttribute(n, v)
+    e
+  }
+
+  implicit def nameFunc(name: String): (Element => Boolean) = _.getNodeName == name
+  implicit def wrapElement(e: Element)(implicit doc: SVGDocument) = new WrapElement(e, doc)
+  implicit def unwrapElement(w: WrapElement): Element = w.domElement
+  implicit def wrapDoc(doc: SVGDocument): DomHelper = new DomHelper(doc)
+  //  implicit
+
+  def dumpNode(doc: Node) = try {
+    import javax.xml.transform._
+    import java.io._
+    val transformer = TransformerFactory.newInstance().newTransformer();
+
+    transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+    transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+
+    val sw = new StringWriter();
+    val sr = new stream.StreamResult(sw);
+    val domSource = new dom.DOMSource(doc);
+    transformer.transform(domSource, sr);
+    sw.toString
+
+  } catch {
+    case e => e.printStackTrace()
+  }
+}
+class DomHelper(doc: SVGDocument) {
+  def element(name: String, args: (String, String)*) = DomHelper.element(doc, name, args: _*)
+  def element(nsName: (String, String), args: (String, String)*) = DomHelper.element(doc, nsName, args: _*)
+
+}
--- src/main/scala/de/immaterialien/qlmap/sprites/ScalableSprite.scala
+++ src/main/scala/de/immaterialien/qlmap/sprites/ScalableSprite.scala
@@ -10,19 +10,21 @@ import GroundClass._
 import scala.collection._
 import org.apache.batik
 import batik.dom.svg
-
+import org.apache.batik.ext.awt.image.renderable._
+import org.apache.batik.gvt.filter._
+import java.awt.image.renderable._
 import java.io._
+import org.w3c.dom._
+import org.w3c.dom.svg._
+import DomHelper._
 
 object ScalableSprite {
   import org.apache.batik.ext.awt.image.renderable._
   val parser = batik.util.XMLResourceDescriptor.getXMLParserClassName
   val factory = new svg.SAXSVGDocumentFactory(parser, false)
   factory.setValidating(false)
-  val toRed = Some(
-      ColorMatrixRable8Bit.buildHueRotate(Math.Pi.toFloat / 3f)
-      
-  )
-  val toBlue = Some(ColorMatrixRable8Bit.buildHueRotate(Math.Pi.toFloat / -3f))
+  val hueRotation = Math.Pi.toFloat / 3f
+
 
   def create(cls: GC, side: Int): Option[Paintable] = {
     val fname: String = (cls match {
@@ -39,24 +41,117 @@ object ScalableSprite {
       try {
         import batik.bridge._
         //val doc = factory.createDocument("http://immaterialien.de/uri/qlmap/sprites#" + fname, stram)
-        val doc = factory.createDocument(null, stram)
+        implicit val doc:SVGDocument = factory.createSVGDocument(null, stram)
+
+        val matrix = {
+          val a = "0.3"
+          def r = if(side==1) " 1 " else " 0 "
+          def b = if(side==2) " 1 " else " 0 "
+          val o = " 0 "
+          val l = " 1 "
+//          ""  
+//          b+r+o+o+a+ "\n" +
+//          r+o+b+o+a+ "\n" +
+//          o+b+r+o+a+ "\n" +
+//          o+o+o+l+o+ "\n" +
+//          ""
+            
+                      ""  
+          b+r+o+" 0    "+a+ " " +
+          r+o+b+" -0.2 "+a+ " " +
+          o+b+r+" 0    "+a+ " " +
+          "1 -1 1 1 0" +
+          ""
+        }
+        {
+          val d = doc.getDocumentElement
+          var n :SVGSVGElement = doc.getRootElement
+//          val children : NodeList = n.getChildNodes
+//          val defs = doc.createElement("defs")
+//          val len = children.getLength
+//          val fs = children.item(1)
+println("n children: "+n.children("g").length)
+          val defs = n.firstElement("defs").getOrElse{
+            val d=doc.element("defs")
+            n.appendBefore("g", d) 
+            d
+          }
+          defs.append(
+                  doc.element("filter",
+//                      "filterUnits"-> "userSpaceOnUse",
+//                      "x"-> "0",
+//                      "y"-> "0",
+//                      "height"-> "64",
+//                      "width"-> "64",
+                      "id"->"MyFilter"
+                  ).append(
+                      doc.element(
+                          "feColorMatrix", 
+                          "type"->"matrix",
+                          "values"->matrix 
+                      )
+                  )
+              )
+          
+          
+          //children.
+          //n.insertBefore(n.getChildNodes.item(0), defs)
+//          val myf = doc.createElement("filter")
+//          myf.setAttribute("id", "MyFilter")
+//          myf.setAttribute("filterUnits","userSpaceOnUse")
+//          myf.setAttribute("x","0" ) 
+//          myf.setAttribute("y","0" ) 
+//          myf.setAttribute("width","64" ) 
+//          myf.setAttribute("height","64")
+//          defs.appendChild(myf)
+          
+//          val gb = doc.createElement("feGaussianBlur")
+//          gb.setAttribute("in", "SourceAlpha") 
+//          gb.setAttribute("stdDeviation", "4") 
+//          gb.setAttribute("result", "blur")
+//          myf.appendChild(gb)
+          
+          val g = {
+            val cs = n.getChildNodes
+            for(i <- 0 until cs.getLength)yield{
+              val c = cs.item(i)
+              c
+            }
+          }.filter(_.getNodeName=="g").first.asInstanceOf[Element]
+          g.setAttribute("filter","url(#MyFilter)")
+        }
+println(dumpNode(doc))        
 
         val userAgent = new UserAgentAdapter()
         val loader = new DocumentLoader(userAgent)
         val ctx = new BridgeContext(userAgent, loader)
         ctx.setDynamicState(BridgeContext.DYNAMIC)
         val builder = new GVTBuilder()
-        
-        
+
         val rootGN = builder.build(ctx, doc)
 //        rootGN.
-        val f = if (side == 1) toRed else if (side == 2) toBlue else None
-        f map (f=>{
-          val af = rootGN.getFilter
-          f.setSource(af)
-          rootGN.setFilter(f)
-        })
+        val f : Option[ColorMatrixRable]= 
+          if (side == 1) Some(ColorMatrixRable8Bit.buildHueRotate(hueRotation * -1)) 
+          else if (side == 2) Some(ColorMatrixRable8Bit.buildHueRotate(hueRotation)) 
+          else None
+        
+        var rable:AbstractRable = new GraphicsNodeRable8Bit(rootGN) 
+        rable = {
+            val r = ColorMatrixRable8Bit.buildSaturate(4f).asInstanceOf[AbstractRable]
+            r.asInstanceOf[ColorMatrixRable].setSource(rable)
+            r
+          }
+        
+        val res = f map (f=>{
+          f.setSource(rable)
+          f
+        }) getOrElse rable
+        
+        
+        
+        
         Some(new ScalableSprite(rootGN))
+//        Some(new RableSprite(res))
       } catch {
         case e => {
           error("error loading " + fname, e)
@@ -72,6 +167,21 @@ object ScalableSprite {
   }
 
 }
+class RableSprite(r: RenderableImage) extends Log with Paintable {
+  import ScalableSprite._
+
+  def paint(trans: AffineTransform, depth: Double, g2d: java.awt.Graphics2D) {
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+//    val i = r.
+    g2d.drawRenderableImage(r, trans)
+//    g2d.drawRenderedImage(r, trans)
+  }
+  def dimensions: (Int, Int) = {
+//    val bds = r.getDimension
+    (r.getWidth.toInt, r.getHeight.toInt)
+  }
+}
+
 class ScalableSprite(r: batik.gvt.GraphicsNode) extends Log with Paintable {
   import ScalableSprite._
 
@@ -86,7 +196,11 @@ class ScalableSprite(r: batik.gvt.GraphicsNode) extends Log with Paintable {
       
     //        new AffineTransform(trans);nt.concatenate(old)
     //r.setFilter()
+//    r.getDocument
+//    r.setFilter(f)
+      
     r.setTransform(nt)
+    
     r.paint(g2d)
     r.setTransform(old)
   }
