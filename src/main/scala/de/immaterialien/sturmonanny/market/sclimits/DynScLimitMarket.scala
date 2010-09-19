85ab647ef9bb26237c46e2c012ab6d68d333e445
usr <github4putty@spam.ulf-schreiber.de>
2010-09-19 22:56:06 +0200

    before moving to fully separated sides

--- src/main/scala/de/immaterialien/sturmonanny/market/sclimits/DynScLimitMarket.scala
+++ src/main/scala/de/immaterialien/sturmonanny/market/sclimits/DynScLimitMarket.scala
@@ -5,10 +5,20 @@ import IMarket._
 import java.io._
 import scala.collection._
 class DynScLimitMarket extends IMarket with Log{ import DynScLimitMarket._
+	val priceFactor = 10D
+	val supplyFactor = 2D // minutes for one SC plane
 	
-	private var pl = Map[Int, Map[String, Double]]()
-	private var supply = Map[Int, Map[String, Double]]()
+//	/**
+//	 * for the initial calculation, any available plane supply information from the previous round will be scaled 
+//	 * so that to total amount of new supply w
+//	 */
+//	val backlogWeight = 1D //    
+	var pl = Map[Int, Map[String, Double]]()
+	var supply = Map[Int, Map[String, Double]]()
 	private var use = Map[Int, Map[String, Variable[Double]]]()
+	//(summe über die supply-maps füllen)
+	var totalsupply = Map[Int, Double]()
+	
 	
 	protected def tryPrice(loadout:Loadout, side:Int) : Option[Double] = {
 log debug "trying price for "+loadout+" in side "+side
@@ -49,56 +59,72 @@ println(".. new value "+plane.v)
 				us<-supply.get(side);
 				supp<-us.get(plane)					
 			) {
-println("adding "+supp+" to "+plane)				
+println("adding "+supp+" to "+plane+" -> "+(variable.v + supp))				
 				variable.v = variable.v + supp
 			}
 		} 
 		
 		pl = for((side, planes)<-use) yield { 
+			val supplyCount:Double = totalsupply(side)
 			val sum = planes.map(_._2.v).foldLeft(0D)(_ + _)
+			val relativeSum = sum/supplyCount
+			
+			val total = planes.size
+
+			val avg = sum / total
+println("side "+side)				
+println("total: "+total+" avg:"+avg+" sum:"+sum +"  _> relativesum: "+relativeSum)				
 			val sideRet = for((plane, variable)<-planes) yield {
 				val count = variable.v
-				var prices = Map[String, Double]()
-				val total = planes.size
-				val avg = sum / total
-				
+//				var prices = Map[String, Double]()
+
 				
 				/* example calculation:
 				 * 
-				 * 10 spit
-				 * 30 hurri
-				 * 
-				 * avg 20
-				 * total 40
-				 * 
-				 * spit: 10 less than average
-				 * hurry: 10 more than average
-				 * 
-				 * but we need 3 times as many hurri in the air:
-				 * 
-				 *                      
-				 * spit:  40/10 = 12/3   =>  3  => 120
-				 * hurry: 40/30 = 4/3    => -1  => -40
-				 * 
-				 * *3:
-				 * spit:  =>  3
-				 * hurry: => -1
-				 *
-				 *  since the exact factor is irrelevant we use 
+
+5 spit
+10 p40
+15 hurri
+
+avg 10
+total 40
+
+spit: 5 less than average
+hurry: 5 more than average
+
+but we need 3 times as many hurri in the air, so h would b1 -1, s would be +3
+
+a/c-1*x:
+
+p40       
+10/10 -> 1 -1 -> 0  *-10 => 0
+               
+spit:  
+10/5 -> 2 -1 -> 1 
+
+hurry: 
+10/15 = 0,66 - 1 -> -0,33
+
 				 */
 
-					val r = if(count==avg)0D else {
-						val sign = if(count > avg) -1D else 1D
-						val factor = 10D
-					
-						sign*factor*(total.toDouble / count.toDouble)
-					}
+				val r = if(count==0D) 0D else {
+					priceFactor * (avg/count - 1)
+				}
+				
+//					val r = if(count==avg)0D else {
+//						val sign = if(count > avg) -1D else 1D
+//						val factor = 10D
+//					
+//						sign*factor*(total.toDouble / count.toDouble) * relativeSum
+//					}
 					(plane,r)
 				}
 //				println(s+" planes proc: "+prices)		
 //				ret = ret + ((s, prices))
 				(side, sideRet)
 			}
+		
+println("recalculated: "+pl)		
 		}
 	
 	
@@ -138,7 +164,7 @@ log debug "cycling to "+iniName
 					side = None
 				}
 				case (Some(_), planeCount(which, is)) => {
-					val i = is.toInt.toDouble
+					val i = is.toInt.toDouble * supplyFactor
 println("add "+side+" plane "+i+" of "+which)					
 					planes.put(which, planes.get(which).map(_ + i).getOrElse(i))
 				}
@@ -148,6 +174,9 @@ println(" ignoring '"+x+"'" )
 			log debug "new supply "+supply
 		}
 		source.close
+		totalsupply = supply.map{kv=>
+			(kv._1, kv._2.values.foldLeft(0D)(_+_))
+		}
 		
 		// create a copy of supply with 0-Variables
 		use = supply.map(x=>(x._1, x._2.map(y=>(y._1, new Variable(0D)))))
